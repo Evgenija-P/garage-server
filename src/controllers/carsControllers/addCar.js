31b52aa2c364e44f03baaaa16c6c51c18f93e3ae
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-05-30 00:59:48 +0300

    update car

--- src/controllers/carsControllers/addCar.js
+++ src/controllers/carsControllers/addCar.js
@@ -1,42 +1,48 @@
-const cloudinary = require("cloudinary").v2;
-const path = require("path")
+const cloudinary = require('cloudinary').v2;
+const path = require('path');
+const { Car } = require('../../models/car');
 
-const {Car} = require("../../models/car")
-const {HttpError} = require("../../helpers/errors");
+const { HttpError } = require('../../helpers/errors');
+const Counter = require('../../helpers/counterModel');
 
-require("dotenv").config();
+require('dotenv').config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET,
-  });
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
-  const addCar = async(req,res,next) => {
-    try {
-      const files = req.files;
-  
-      const photo_urls = req.files.map(file => {
-        return `/cars/${file.filename}`;
-      });
-      
-      const primary_photo_url = photo_urls[0];
-      
-      const newCar = await Car.create({
-        ...req.body,
-        photo_urls,
-        primary_photo_url
-      });
-      
-  
-      res.status(201).json({
-        message: "success",
-        newCar
-      });
-    } catch (error) {
-      next(error);
-    }
-  };
-  
+const addCar = async (req, res, next) => {
+  try {
+    const files = req.files;
 
-module.exports = addCar
+    const photo_urls = files.map(file => `/cars/${file.filename}`);
+    const primary_photo_url = photo_urls[0];
+
+    // Отримати новий ID
+    const counter = await Counter.findOneAndUpdate(
+      { name: 'car' },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+
+    const custom_id = counter.seq;
+
+    const newCar = await Car.create({
+      custom_id,
+      ...req.body,
+      photo_urls,
+      primary_photo_url,
+    });
+
+    res.status(201).json({
+      message: 'success',
+      newCar,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = addCar;
